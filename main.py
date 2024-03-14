8007747db3f284b8e4caa65bc8961cf0202fb194
thomaoc <thomas.ocuilleanain@ulb.be>
2024-03-14 20:23:12 +0100

    Removed warning and added predicted label as image title for swag purposes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from datasets import load_dataset
 
-dataset = load_dataset("huggingface/cats-image")
+dataset = load_dataset("huggingface/cats-image", trust_remote_code=True)
 image = dataset["test"]["image"][0]
 
 image_processor = AutoImageProcessor.from_pretrained("google/vit-base-patch16-224")
@@ -15,10 +15,11 @@ inputs = image_processor(image, return_tensors="pt")
 with torch.no_grad():
     logits = model(**inputs).logits
 
-# Show the image
-plt.imshow(np.array(image))
-plt.show()
-
 # model predicts one of the 1000 ImageNet classes
 predicted_label = logits.argmax(-1).item()
-print(model.config.id2label[predicted_label])
+label = model.config.id2label[predicted_label]
+
+# Show the image with caption of predicted label
+plt.imshow(np.array(image))
+plt.title(f'Prediction: {label}')
+plt.show()
