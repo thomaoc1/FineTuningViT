583d1c11e68f1e5290a6863266d24eae6b931f13
thomaoc <thomas.ocuilleanain@ulb.be>
2024-03-14 20:20:55 +0100

    Pretrained ViT using hugging face api & requirements

---
+++ main.py
@@ -0,0 +1,24 @@
+from transformers import AutoImageProcessor, ViTForImageClassification
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
+from datasets import load_dataset
+
+dataset = load_dataset("huggingface/cats-image")
+image = dataset["test"]["image"][0]
+
+image_processor = AutoImageProcessor.from_pretrained("google/vit-base-patch16-224")
+model = ViTForImageClassification.from_pretrained("google/vit-base-patch16-224")
+
+inputs = image_processor(image, return_tensors="pt")
+
+with torch.no_grad():
+    logits = model(**inputs).logits
+
+# Show the image
+plt.imshow(np.array(image))
+plt.show()
+
+# model predicts one of the 1000 ImageNet classes
+predicted_label = logits.argmax(-1).item()
+print(model.config.id2label[predicted_label])
